8b23d166fe3bc91f05852034f2d62db41e1df7d9
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-06-22 10:33:19 +1000

    Initial Commit: setup next js with material ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { StyledRoot } from './StyledRoot';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AppRouterCacheProvider>
-          {children}
+           <StyledRoot>{children}</StyledRoot>
         </AppRouterCacheProvider>
       </body>
     </html>
--- app/theme.ts
+++ app/theme.ts
@@ -2,6 +2,7 @@
 'use client';
 import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
+import { red, blue } from '@mui/material/colors';
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -12,6 +13,14 @@ const roboto = Roboto({
 const theme = createTheme({
   typography: {
     fontFamily: roboto.style.fontFamily,
+  },
+  palette: {
+    primary: {
+      main: blue[700], // #1976d2 by default
+    },
+    secondary: {
+      main: red[500], // optional secondary
+    },
   },
 });
 
